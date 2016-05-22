dcf98dfa02dc853ef3dfb959e1f22eb5fe37ecc3
Pablo Fernandez <pablofveiga@gmail.com>
2016-05-22 18:40:31 +0200

    styles_update

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,51 +1,145 @@
-function get_todos() {
-    var todos = new Array;
-    var todos_str = localStorage.getItem('todo');
-    if (todos_str !== null) {
-        todos = JSON.parse(todos_str); 
+'use strict';
+
+
+function getJSON(src) {
+    var request = new XMLHttpRequest();
+    request.open('GET', src, false); 
+    request.send(null);
+
+    if (request.status === 200) {
+        try {
+            return JSON.parse(request.responseText);
+        } catch(e) {
+            return null;
+        }
     }
-    return todos;
+    return null;
 }
- 
-function add() {
-    var task = document.getElementById('task').value;
- 
-    var todos = get_todos();
-    todos.push(task);
-    localStorage.setItem('todo', JSON.stringify(todos));
- 
-    show();
- 
-    return false;
+
+function order(obj,todos) {
+    if(todos.sort_by === 'newest_first') {
+        obj.sort(function(a,b){
+          return a.updated_at < b.updated_at;
+        });
+    }
+    return obj;
 }
- 
-function remove() {
-    var id = this.getAttribute('id');
-    var todos = get_todos();
-    todos.splice(id, 1);
-    localStorage.setItem('todo', JSON.stringify(todos));
- 
+
+function filter (obj) {
+    if(!obj.filter_by) return obj.items;
+
+    var result = [];
+    for(let todo of obj.items) {
+        if(todo.status === obj.filter_by) result.push(todo);
+    }
+
+    return result;
+}
+
+function loadToDo(src){
+    var localTodo = getLocalStorage('todo');
+
+    if(localTodo) return localTodo;
+
+    var todosFile = getJSON(src);
+    setLocalStorage('todo', todosFile);
+
+    return todosFile;
+}
+
+function getLocalStorage(name) {
+    var todos_str = localStorage.getItem(name);
+
+    if (todos_str) return JSON.parse(todos_str); 
+    return;
+}
+
+function setLocalStorage (name, value) {
+    localStorage.setItem(name,JSON.stringify(value));
+}
+
+function action(type, id){
+    for(let todo of todos.items) {
+        if(todo.updated_at == id) todo.status = type;
+    }
+
+    setLocalStorage('todo', todos);
     show();
- 
-    return false;
+}
+
+function add(){
+    var newTask = document.getElementById('newTask');
+    if(!newTask.value) return;
+    
+    var date = new Date();
+    var time = date.getTime();
+
+    todos.items.push({
+        updated_at: time,
+        description: newTask.value,
+        status: "todo"
+    });
+
+    setLocalStorage('todo', todos);
+    newTask.value = '';
+    show();
+}
+
+function selectedOption(){
+    if(todos.filter_by === 'todo') document.getElementById("link-todo").className = 'link-selected';
+    else if(todos.filter_by === 'done') document.getElementById("link-done").className = 'link-selected';
+    else if(todos.filter_by === 'removed') document.getElementById("link-removed").className = 'link-selected';
+    else document.getElementById("link-all").className = 'link-selected';
+}
+
+function resetSelected(){
+    document.getElementById("link-all").className = '';
+    document.getElementById("link-todo").className = '';
+    document.getElementById("link-done").className = '';
+    document.getElementById("link-removed").className = '';
+}
+
+function showFiltered(element, type) {
+    resetSelected();
+
+    todos.filter_by = type;
+    element.className += 'link-selected';
+    setLocalStorage('todo', todos);
+    show();
 }
  
 function show() {
-    var todos = get_todos();
- 
-    var html = '<ul>';
-    for(var i=0; i<todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-    };
+
+    var items = todos.items;
+    items = filter(todos);
+    items = order(items, todos);
+
+    if(!todos.items) return;
+
+    var html = '<ul class="task-display">';
+
+    for(let todo of items) {
+
+        html += `<li class="${todo.status} task-item">
+                    <span class="description">${todo.description}</span>
+                    <ul class="button-container">
+                        <li><a href="#" title="Done" class="icon-done" onclick="action('done', ${todo.updated_at})"></a></li>
+                        <li><a href="#" title="Remove" class="icon-remove" onclick="action('removed', ${todo.updated_at})"></a></li>
+                    </ul>
+                </li>`;
+    }
+
     html += '</ul>';
  
-    document.getElementById('todos').innerHTML = html;
- 
-    var buttons = document.getElementsByClassName('remove');
-    for (var i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
-    };
+    document.getElementById('todo-list').innerHTML = html;
 }
- 
-document.getElementById('add').addEventListener('click', add);
-show();
+
+var todos = {};
+
+function init(){
+    todos = loadToDo('js/data.json');
+    selectedOption();
+    show();
+}
+
+window.onload = init;
